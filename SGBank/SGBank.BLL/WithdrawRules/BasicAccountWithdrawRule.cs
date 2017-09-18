077e07819d07d93c4300db65ddc8f1b293b5bafc
Jarid Wagner <wagnerjarid@gmail.com>
2017-09-18 10:52:04 -0500

    SG BANK partial

---
+++ SGBank/SGBank.BLL/WithdrawRules/BasicAccountWithdrawRule.cs
@@ -0,0 +1,60 @@
+﻿using SGBank.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGBank.Models;
+using SGBank.Models.Responses;
+
+namespace SGBank.BLL.WithdrawRules
+{
+    public class BasicAccountWithdrawRule : IWithdraw
+    {
+        public AccountWithdrawResponse Withdraw(Account account, decimal amount)
+        {
+            AccountWithdrawResponse response = new AccountWithdrawResponse();
+
+            if (account.Type != AccountType.Basic)
+            {
+                response.Success = false;
+                Console.WriteLine("Error: a non-basic account hit the Basic Withdraw Rule. Contact IT");
+                return response;
+            }
+            if (amount >= 0)
+            {
+                response.Success = false;
+                Console.WriteLine("Withdrawal amounts must be negative!");
+                return response;
+            }
+            if (amount < -500)
+            {
+                response.Success = false;
+                Console.WriteLine("Basic accounts cannot withdraw more than $500!");
+                return response;
+            }
+            if ((amount + account.Balance) < -100)
+            {
+                response.Success = false;
+                Console.WriteLine("This amount will overdraft more than your $100 limit!");
+                return response;
+            }
+            else
+            {
+                response.Success = true;
+                response.Account = account;
+                response.Amount = amount;
+                response.OldBalance = account.Balance;
+                account.Balance = account.Balance - amount;
+                if(account.Balance < 0)
+                {
+                    account.Balance = account.Balance - 10;
+                    Console.WriteLine("Your account balance was negative so we took another ten bucks lolol");
+                }
+                return response;
+            }
+        }
+
+    }
+}
+
---
+++ SGBank/SGBank.BLL/WithdrawRules/WithdrawRulesFactory.cs
@@ -0,0 +1,30 @@
+﻿using SGBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGBank.Models.Interfaces;
+
+namespace SGBank.BLL.WithdrawRules
+{
+    class WithdrawRulesFactory
+    {
+        public static IWithdraw Create(AccountType Account)
+        {
+            switch (Account)
+            {
+                case AccountType.Free:
+                    return new FreeAccountWithdrawRule();
+                case AccountType.Basic:
+                    return new BasicAccountWithdrawRule();
+          
+
+                default:
+                    Console.WriteLine("Account type is not supported!");
+                    throw new Exception();
+
+            }
+        }
+    }
+}
