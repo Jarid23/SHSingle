fa443dd99ef08ae4b7a322a4863bad89cdd3672d
Jarid Wagner <wagnerjarid@gmail.com>
2017-09-21 14:35:59 -0500

    Newest

---
+++ FlooringMastery/FlooringMastery.UI/ConsoleIO.cs
@@ -0,0 +1,29 @@
+﻿using FlooringMastery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.UI
+{
+    public class ConsoleIO
+    {
+        public static void DisplayOrderDetails(Order order)
+        {
+            Console.WriteLine($"Order: {order.CustomerName}");
+            Console.WriteLine($"Total: {order.Total:c}");
+        }
+        public static DateTime GetDateTime(string date)
+        {  
+                     
+            DateTime dt = Convert.ToDateTime(date);            
+            return dt;
+        }
+
+        internal static void DisplayOrdersDetails(List<Order> orders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
