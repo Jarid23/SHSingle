ecc37060b5f9ee12ae4b1e1e7ed07571ad3cc6ed
Jarid Wagner <wagnerjarid@gmail.com>
2017-10-04 16:30:18 -0500

    Mastery Done

--- FlooringMastery/3FlooringMastery/FlooringMastery.UI/FlooringMastery.Tests/EditTest.cs
+++ FlooringMastery/3FlooringMastery/FlooringMastery.UI/FlooringMastery.Tests/EditTest.cs
@@ -1,38 +1,69 @@
-﻿//using FlooringMastery.BLL;
-//using FlooringMastery.Data;
-//using FlooringMastery.Models;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using FlooringMastery.BLL;
+using FlooringMastery.Data;
+using FlooringMastery.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace FlooringMastery.Tests
-//{
-//    [TestFixture]
-//    public class EditTest
-//    {
-//        [TestCase(1, "05/05/2020", "Jarid", "OH", 5, "Wood", 101, 5.15, 4.75)]
-//        public void EditTestMethod(int orderNumber, string orderDate, string customerName, string state, decimal taxRate, string productType, decimal area, decimal costPerSquareFoot, decimal laborCostPerSquareFoot)
-//        {
-//            var manager = new OrderManager(new LiveDataRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\"), new ProductRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\Products.txt"), new StateRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\Taxes.txt"));
+namespace FlooringMastery.Tests
+{
+    [TestFixture]
+    public class EditTest
+    {
+        [TestCase(1, "05/05/2020", "Jarid", "OH", 5, "Wood", 101, 5.15, 4.75)]
+        public void EditTestMethod(int orderNumber, string orderDate, string customerName, string state, decimal taxRate, string productType, decimal area, decimal costPerSquareFoot, decimal laborCostPerSquareFoot)
+        {
+            var manager = new OrderManager(new LiveDataRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\"), new ProductRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\Products.txt"), new StateRepository(@"C:\Users\jwagner\Desktop\REPOS\dotnet---jarid---wagner\FlooringMastery\3FlooringMastery\FlooringMastery.UI\Data\Taxes.txt"));
 
-//            Order orderVariable = new Order()
-//            {
+            Order orderVariable = new Order()
+            {
 
-//                OrderNumber = orderNumber,
-//                OrderDate = DateTime.Parse(orderDate),
-//                CustomerName = customerName,
-//                State = state,
-//                TaxRate = taxRate,
-//                ProductType = productType,
-//                Area = area,
-//                CostPerSquareFoot = costPerSquareFoot,
-//                LaborCostPerSquareFoot = laborCostPerSquareFoot
-//            };
-            
-//        }
+                OrderNumber = orderNumber,
+                OrderDate = DateTime.Parse(orderDate),
+                CustomerName = customerName,
+                State = state,
+                TaxRate = taxRate,
+                ProductType = productType,
+                Area = area,
+                CostPerSquareFoot = costPerSquareFoot,
+                LaborCostPerSquareFoot = laborCostPerSquareFoot
+            };
+            manager.AddOrder(orderVariable);
 
-//    }
-//}
+            Assert.AreEqual(orderVariable.CustomerName, customerName);
+            Assert.AreEqual(orderVariable.State, state);
+            Assert.AreEqual(orderVariable.Area, area);
+          
+
+            var EditedName = "Eric";
+            var EditedState = "PA";
+            var EditedArea = 102;
+            var EditedProduct = "Laminate";
+
+            Order secondOrderVariable = new Order()
+            {
+                OrderNumber = orderVariable.OrderNumber,
+                OrderDate = orderVariable.OrderDate,
+                CustomerName = EditedName,
+                State = EditedState,
+                TaxRate = 6,
+                ProductType = EditedProduct,
+                Area = EditedArea,
+                CostPerSquareFoot = 5,
+                LaborCostPerSquareFoot = 5
+            };
+            Assert.AreEqual(orderVariable.OrderNumber, secondOrderVariable.OrderNumber);
+            manager.RemoveOrder(orderVariable);
+            manager.AddOrder(secondOrderVariable);
+
+            Assert.AreEqual(secondOrderVariable.CustomerName, EditedName);
+            Assert.AreEqual(secondOrderVariable.State, EditedState);
+            Assert.AreEqual(secondOrderVariable.Area, EditedArea);
+            manager.RemoveOrder(secondOrderVariable);
+        }
+        
+    }
+}
