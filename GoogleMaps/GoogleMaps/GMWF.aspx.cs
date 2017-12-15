ebfa9a51b595fc48f4bd32fc423932c2b8df85f6
Jarid Wagner <wagnerjarid@gmail.com>
2017-12-15 16:21:47 -0600

    added images

---
+++ GoogleMaps/GoogleMaps/GMWF.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace GoogleMaps
+{
+    public partial class GMWF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string city = TextBox1.Text;
+            string state = TextBox2.Text;
+            string country = TextBox3.Text;
+
+            StringBuilder add = new StringBuilder("https://www.google.com/maps?q=");
+
+            add.Append(city);
+            add.Append(state);
+            add.Append(country);
+
+            
+
+
+
+        }
+    }
+}
