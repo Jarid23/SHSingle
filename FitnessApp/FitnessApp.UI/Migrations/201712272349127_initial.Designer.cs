ed42b3fad2f6489e4b7733555524e5c7ec41136c
Jarid Wagner <wagnerjarid@gmail.com>
2017-12-27 17:52:58 -0600

    EF Working

--- FitnessApp/FitnessApp.UI/Migrations/201712272349127_initial.Designer.cs
+++ FitnessApp/FitnessApp.UI/Migrations/201712272349127_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace FitnessApp.UI.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class init2 : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201712271637256_init2"; }
+            get { return "201712272349127_initial"; }
         }
         
         string IMigrationMetadata.Source
